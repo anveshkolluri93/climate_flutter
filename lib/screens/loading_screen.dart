eb27ebd0752946a83e1c342c7d15c18e96001a17
“anveshkumar” <anveshkolluri93@gmail.com>
2019-09-06 20:24:04 -0500

    parse response json

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -24,7 +25,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
     http.Response response = await http.get(
         'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
     if (response.statusCode == 200) {
-      print(response.body);
+      String data = response.body;
+
+      var decodedData = jsonDecode(
+          data); // 'var becoz output is a dynamic type for jsonDynamic
+
+      double temp = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String tem = decodedData['name'];
+      print(temp);
     } else {
       print(response.statusCode);
     }
