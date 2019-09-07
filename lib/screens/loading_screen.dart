b2cf2811766a4d296968d1ba92b7e56351d14e9a
“anveshkumar” <anveshkolluri93@gmail.com>
2019-09-07 11:36:34 -0500

    Gets live data of the user

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+const apiKey = 'b6dde4b5af1ce9bd96abb61cbcae5425';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude, longitude;
   @override
   void initState() {
     super.initState();
@@ -18,12 +21,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = new Location();
     await location.getCurrentLocation();
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+    getData();
   }
 
   void getData() async {
     http.Response response = await http.get(
-        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
     if (response.statusCode == 200) {
       String data = response.body;
 
@@ -34,6 +39,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
       int condition = decodedData['weather'][0]['id'];
       String tem = decodedData['name'];
       print(temp);
+      print(condition);
+      print(tem);
     } else {
       print(response.statusCode);
     }
@@ -41,7 +48,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold(
       body: Center(),
     );
